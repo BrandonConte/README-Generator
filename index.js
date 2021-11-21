c462cb02f4b592c8336722bfdf93c622a6209914
BrandonConte <bconte950@gmail.com>
2021-11-21 10:30:41 -0500

    added in constants and beginning of prompt

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+
+const generateFile = require('./utils/');
+
+const generateMD = require('./images/');
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+
+        }
+    ])
+}
